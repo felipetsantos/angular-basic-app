579d20baf40b90800c28fb888d332ff05261e39b
Felipe Santos <felipe.feteles@gmail.com>
2017-07-19 16:45:16 -0300

    Select recipe, and change pages

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import {	Recipe	} from '../recipe.model';
 
@@ -9,13 +9,17 @@ import {	Recipe	} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	recipes: Recipe[] = [
-		new Recipe('A Test Recipe',	'This is a simply test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-		new Recipe('A Test Recipe',	'This is a simply test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+		new Recipe('Salmon',	'This is a simply test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+		new Recipe('Chicken Recipe',	'this is a amazing Chicken recipe', 'http://www.seriouseats.com/recipes/assets_c/2015/12/20151218-braised-chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg'),
 	];
-
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe)  {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
